8cc645719761e4224d32956b6a73f4f1965c852a
Mario Moraes <moraes_mario@hotmail.com>
2023-05-09 15:53:12 -0300

    Config

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 import '../../core/env/env.dart';
+import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/message.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Message {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +19,20 @@ class HomePage extends StatelessWidget {
         title: const Text('Home Page'),
       ),
       body: SizedBox(
-        child: Text(Env.instance.get('backend_base_url')),
+        child: Text(
+          Env.instance.get('backend_base_url'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showLoader();
+          await Future.delayed(const Duration(seconds: 3));
+          hideLoader();
+
+          // showSuccess('Valeuuuuu Rita Lee!');
+          showError('Error');
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
