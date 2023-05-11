aafa9cab65227629ce56d313f254d6afefa8c646
Mario Moraes <moraes_mario@hotmail.com>
2023-05-11 18:03:34 -0300

    Upgrade Version

--- lib/src/modules/home/login/login_page.dart
+++ lib/src/modules/home/login/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../core/ui/helpers/size_extensions.dart';
 import '../../../core/ui/styles/colors_app.dart';
+import '../../../core/ui/styles/text_styles.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -27,14 +28,67 @@ class LoginPage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            constraints: BoxConstraints(
-              maxHeight: context.percentHeigth(.5),
-              maxWidth: context.percentHeigth(screenWidth < 1300 ? .7 : .3),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              constraints: BoxConstraints(
+                maxHeight: context.percentHeigth(.5),
+                maxWidth: context.percentHeigth(screenWidth < 1300 ? .7 : .45),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      FractionallySizedBox(
+                        widthFactor: .3,
+                        child: Image.asset(
+                          'assets/images/logo.png',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: Text('LOGIN', style: context.textStyle.textTile),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Senha',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('ENTRAR'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
           )
         ],
